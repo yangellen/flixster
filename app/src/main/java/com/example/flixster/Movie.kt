f57f582052a55b65e26c3e17089a5195e59690d7
Ellen Yang <yange@oregonstate.edu>
2022-02-25 23:07:46 -0800

    feat: make movie api request and parse JSON

---
+++ app/src/main/java/com/example/flixster/Movie.kt
@@ -0,0 +1,30 @@
+package com.example.flixster
+
+import org.json.JSONArray
+
+data class Movie (
+    val movieId: Int,
+    val posterPath: String,
+    val title: String,
+    val overview:String,
+) {
+    companion object {
+        fun fromJsonArray(movieJsonArray: JSONArray): List<Movie> {
+            val movies = mutableListOf<Movie>()
+            for (i in 0 until movieJsonArray.length()){
+                val movieJson = movieJsonArray.getJSONObject(i)
+                movies.add(
+                    Movie(
+                        movieJson.getInt("id"),
+                        movieJson.getString("poster_path"),
+                        movieJson.getString("title"),
+                        movieJson.getString("overview")
+
+                    )
+                )
+            }
+            return movies
+        }
+    }
+}
+
