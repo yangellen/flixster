db68293407da6c4ef47cf6ee895ad47241297756
Ellen Yang <yange@oregonstate.edu>
2022-03-04 21:25:19 -0800

    feat: expose details of movie in a separte activity

---
+++ app/src/main/java/com/example/flixster/DetailActivity.kt
@@ -0,0 +1,28 @@
+package com.example.flixster
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.RatingBar
+import android.widget.TextView
+
+private const val TAG = "DetailActivity"
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var tvTitle: TextView
+    private lateinit var tvOverview: TextView
+    private lateinit var ratingBar: RatingBar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+        tvTitle = findViewById(R.id.tvTitle)
+        tvOverview = findViewById(R.id.tvOverview)
+        ratingBar = findViewById(R.id.rbVoteAverage)
+
+        val movie = intent.getParcelableExtra<Movie>(MOVIE_EXTRA) as Movie
+        tvTitle.text = movie.title
+        tvOverview.text = movie.overview
+        ratingBar.rating = movie.voteAverage.toFloat()
+    }
+}
--- app/src/main/java/com/example/flixster/Movie.kt
+++ app/src/main/java/com/example/flixster/Movie.kt
@@ -1,14 +1,22 @@
 package com.example.flixster
 
+import android.os.Parcelable
+import kotlinx.parcelize.IgnoredOnParcel
+import kotlinx.parcelize.Parcelize
 import org.json.JSONArray
+
+@Parcelize
 
 data class Movie (
     val movieId: Int,
+    val voteAverage: Double,
     private val posterPath: String,
     val title: String,
     val overview:String,
-) {
+) : Parcelable {
+    @IgnoredOnParcel
     val posterImageUrl = "https://image.tmdb.org/t/p/w342/$posterPath"
+
     companion object {
         fun fromJsonArray(movieJsonArray: JSONArray): List<Movie> {
             val movies = mutableListOf<Movie>()
@@ -17,6 +25,7 @@ data class Movie (
                 movies.add(
                     Movie(
                         movieJson.getInt("id"),
+                        movieJson.getDouble("vote_average"),
                         movieJson.getString("poster_path"),
                         movieJson.getString("title"),
                         movieJson.getString("overview")
