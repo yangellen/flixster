207f9b6cf11a7d8098a76de6e1b71795ebfa6471
Ellen Yang <yange@oregonstate.edu>
2022-03-04 22:35:54 -0800

    allow video posts to be in full-screen using the YouTubePlayerView

--- app/src/main/java/com/example/flixster/DetailActivity.kt
+++ app/src/main/java/com/example/flixster/DetailActivity.kt
@@ -1,17 +1,27 @@
 package com.example.flixster
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.RatingBar
 import android.widget.TextView
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubePlayerView
+import okhttp3.Headers
 
+
+private const val YOUTUBE_API_KEY = "AIzaSyBWdxBog2fbOlvypTSei9BVtt2arVlh_6s"
+private const val TRAILER_URL = "https://api.themoviedb.org/3/movie/%d/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"
 private const val TAG = "DetailActivity"
-class DetailActivity : AppCompatActivity() {
+class DetailActivity : YouTubeBaseActivity() {
 
     private lateinit var tvTitle: TextView
     private lateinit var tvOverview: TextView
     private lateinit var ratingBar: RatingBar
+    private lateinit var ytPlayerView: YouTubePlayerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,10 +29,60 @@ class DetailActivity : AppCompatActivity() {
         tvTitle = findViewById(R.id.tvTitle)
         tvOverview = findViewById(R.id.tvOverview)
         ratingBar = findViewById(R.id.rbVoteAverage)
+        ytPlayerView = findViewById(R.id.player)
 
         val movie = intent.getParcelableExtra<Movie>(MOVIE_EXTRA) as Movie
         tvTitle.text = movie.title
         tvOverview.text = movie.overview
         ratingBar.rating = movie.voteAverage.toFloat()
+
+        val client = AsyncHttpClient()
+        client.get(TRAILER_URL.format(movie.movieId), object:JsonHttpResponseHandler(){
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                response: String?,
+                throwable: Throwable?
+            ) {
+                Log.i(TAG,"onFailure $statusCode")
+            }
+
+            override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON) {
+                Log.i(TAG, "onSuccess")
+                val results = json.jsonObject.getJSONArray("results")
+                if (results.length() == 0){
+                    Log.w(TAG, "No movie trailers found")
+                    return
+                }
+                val movieTrailerJson = results.getJSONObject(0)
+                val youtubeKey = movieTrailerJson.getString("key")
+
+                initializeYouTube(youtubeKey)
+            }
+        })
+    }
+
+    private fun initializeYouTube(youtubeKey: String) {
+        ytPlayerView.initialize(
+            YOUTUBE_API_KEY,
+            object : YouTubePlayer.OnInitializedListener {
+                override fun onInitializationSuccess(
+                    provider: YouTubePlayer.Provider?,
+                    player: YouTubePlayer?,
+                    p2: Boolean
+                ) {
+                    Log.i(TAG, "onInitializationSuccess")
+                    player?.cueVideo(youtubeKey);
+                }
+
+                override fun onInitializationFailure(
+                    p0: YouTubePlayer.Provider?,
+                    p1: YouTubeInitializationResult?
+                ) {
+                    Log.i(TAG, "onInitializationFailure")
+                }
+
+            })
+
     }
 }
